dd3ac8303ef93af80adc5794630966fe047149db
Mike Decker <pookmish@gmail.com>
2018-12-20 12:07:29 -0800

    dont use decanter template

--- docroot/modules/humsci/hs_layouts/src/Plugin/Layout/HumsciLayout.php
+++ docroot/modules/humsci/hs_layouts/src/Plugin/Layout/HumsciLayout.php
@@ -36,7 +36,7 @@ class HumsciLayout extends LayoutDefault implements PluginFormInterface {
       '#empty_option' => $this->t('Full Width'),
       '#default_value' => $this->configuration['section_width'] ?: NULL,
       '#options' => [
-        'centered-container' => $this->t('Limited Width'),
+        'decanter-grid' => $this->t('Limited Width'),
       ],
     ];
 
